5f2a3320290236a49163279abaad3b459f8b2f83
Anton <antony2bs2lol@gmail.com>
2021-04-03 15:17:12 +0300

    Final version

---
+++ main.py
@@ -0,0 +1,20 @@
+a = 'sdkjfhlazxnmcbzmnxbcmznxbcmznxbcae'
+
+
+def founding(b):
+    maximum = ''
+    for i in b:
+        if b.count(i) == len(b):
+            maximum = b
+            break
+        flag = i
+        b2 = b.split(flag)
+        if len(b2) > 2:
+            b2 = sorted(b2)
+            if len(b2[-1]) > len(maximum):
+                maximum = flag + b2[-1] + flag
+    return maximum
+
+
+answer = founding(a)
+print(answer)
