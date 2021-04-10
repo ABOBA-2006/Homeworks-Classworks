c810ac8852f7c9b68b27979a3c91ae4c9eb9ed3e
Anton <antony2bs2lol@gmail.com>
2021-04-10 15:22:31 +0300

    v 2.0

--- gleb_example_2.py
+++ gleb_example_2.py
@@ -10,23 +10,21 @@ def longest(stroka):
             flag = False
 
     if flag:
+        ind = 0
         for i in range(len(stroka)):
             j = i + 1
-            while j < len(stroka):
+            while j < len(stroka) and stroka.count(stroka[i]) > 1:
                 if stroka[j] == stroka[i]:
-                    break
-                if j == len(stroka) - 1 and stroka[j] != stroka[i]:
-                    j = i + 1
-                    break
+                    ind = j
                 j += 1
-            long_prev = j - i
+            long_prev = ind - i
             if long < long_prev:
                 long = j - i
                 ind_m_s = i
-                ind_m_e = j
+                ind_m_e = ind
         print(stroka[ind_m_s:ind_m_e+1])
     else:
         print('')
 
 
-longest('ababa')
+longest('abcabeba')
