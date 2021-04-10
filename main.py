674fcbbd09352014cb398a9b1f598147e89ecb98
Anton <antony2bs2lol@gmail.com>
2021-04-10 11:30:49 +0300

    v 1.1

--- main.py
+++ main.py
@@ -2,16 +2,26 @@ def func(filename):
     with open(filename, 'r') as file:
         lines = file.readlines()
         i = 0
-        while i <= len(lines):
+        while i < len(lines):
             data = yield lines[i]
             if data == 'next':
                 i += 1
+            if data == 'previous' and i > 0:
+                i -= 1
 
 
 generator_3000 = func('test.txt')
 next(generator_3000)
 
 print(next(generator_3000))
+generator_3000.send('next')
+print(next(generator_3000))
+generator_3000.send('previous')
+print(next(generator_3000))
+print(next(generator_3000))
+print(next(generator_3000))
+generator_3000.send('next')
+generator_3000.send('next')
 generator_3000.send('next')
 print(next(generator_3000))
 
