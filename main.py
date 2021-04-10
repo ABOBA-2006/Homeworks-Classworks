a9e37a5472958a116c25cf41b56d92674cad1857
Anton <antony2bs2lol@gmail.com>
2021-04-10 11:36:33 +0300

    v 2.0

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def func(filename):
         lines = file.readlines()
         i = 0
         while i < len(lines):
-            data = yield lines[i]
+            data = yield lines[i][0:len(lines[i])-1]
             if data == 'next':
                 i += 1
             if data == 'previous' and i > 0:
@@ -22,7 +22,11 @@ print(next(generator_3000))
 print(next(generator_3000))
 generator_3000.send('next')
 generator_3000.send('next')
+print(next(generator_3000))
 generator_3000.send('next')
+print(next(generator_3000))
+generator_3000.send('previous')
+generator_3000.send('previous')
 print(next(generator_3000))
 
 
